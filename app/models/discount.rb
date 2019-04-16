7eb674f1f821e5ba3be9f01f5a1d4ffd2a2c8b0a
Chi Tran <chidtran@gmail.com>
2019-04-16 14:13:44 -0600

    Adds validations and relationships

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,2 +1,15 @@
 class Discount < ApplicationRecord
+  belongs_to :user, foreign_key: 'merchant_id'
+
+  validates :amount_off, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+
+  validates :min_subtotal_required, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  } 
+
+  validates_presence_of :quantity
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Discount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe 'validations' do
+    it { should validate_presence_of :amount_off }
+    it { should validate_numericality_of(:amount_off).only_integer }
+    it { should validate_numericality_of(:amount_off).is_greater_than_or_equal_to(0) }
+    it { should validate_presence_of :min_subtotal_required }
+    it { should validate_numericality_of(:min_subtotal_required).only_integer }
+    it { should validate_numericality_of(:min_subtotal_required).is_greater_than_or_equal_to(0) }
+    it { should validate_presence_of :quantity }
+  end 
+
+  describe 'relationships' do
+    it { should belong_to :user }
+  end 
+end 
