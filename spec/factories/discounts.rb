d6b7c5b41da05cc33f069d062787b9fb04721a56
Chi Tran <chidtran@gmail.com>
2019-04-16 18:32:36 -0600

    Add factorybot for discounts

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :discount do
-    
-  end
-end
+    association :user, factory: :merchant
+    amount_off { 5 }
+    quantity { "" }
+    item_total { 50 }
+    kind { 2 }
+    merchant_id {nil}
+  end 
+end 
